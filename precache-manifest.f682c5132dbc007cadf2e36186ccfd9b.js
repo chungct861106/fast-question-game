81ed9d5944b7e9e0e461fb7a81d73d87e0054e17
chungct861106 <thomson861106@gmail.com>
2021-12-29 02:29:51 +0800

    Updates

---
+++ precache-manifest.f682c5132dbc007cadf2e36186ccfd9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6cfe19e9ee8c531f1f4",
+    "url": "/static/css/main.b2d4063a.chunk.css"
+  },
+  {
+    "revision": "a6cfe19e9ee8c531f1f4",
+    "url": "/static/js/main.a6cfe19e.chunk.js"
+  },
+  {
+    "revision": "cfe4b1b83f91cabb4924",
+    "url": "/static/css/1.2fe6a835.chunk.css"
+  },
+  {
+    "revision": "cfe4b1b83f91cabb4924",
+    "url": "/static/js/1.cfe4b1b8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "804b818a6ac66dc70e7c398dc9ea3f01",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fast-question-game/precache-manifest.2f4140a67028dac81261194cfa2c2021.js"
+  "/precache-manifest.f682c5132dbc007cadf2e36186ccfd9b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/fast-question-game/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
